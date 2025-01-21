f142f468be6606e77b3ac005af5967f03a09215d
Maarten Dekker <maarten.dekker@capgemini.com>
2025-01-21 08:09:54 +0100

    Better instructions

--- src/App.js
+++ src/App.js
@@ -13,15 +13,17 @@ function App() {
         <p>Use your skills to fill the grid with blocks, using code!</p>
         <ul>
           <li>Young professionals: however you like</li>
-          <li>
-            Medior+: delay between the displaying of the blocks, and randomize
-            the locations
-          </li>
-          <li>Bonus points: randomize the grid locations</li>
+          <li>Medior+: delay between the displaying of the blocks</li>
+          <li>Bonus points: randomize! (e.g., the blocks order or colors)</li>
         </ul>
         <Grid>
           <Block />
         </Grid>
+        <p>
+          <em>
+            Note: there is no actual grid, those are just css background lines.
+          </em>
+        </p>
       </main>
     </div>
   );
